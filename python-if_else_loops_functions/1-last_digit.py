21c367f243f2066e38a118e7017e9ea268f6130b
Cyprien <cypriengehu@example.com>
2025-01-15 11:15:10 +0000

    correction

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,11 @@ if number < 0:
     last = number * -1 % 10
 else:
     last = number % 10
+print(f"Last digit of {number} is {last}", end=" ")
 
 if last > 5:
-    print(f"Last digit of {number} is {last} and is greater than 5")
+    print("and is greater than 5")
 elif last == 0:
-    print(f"Last digit of {number} is {last} and is 0")
+    print("and is 0")
 else:
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if ord("e") != i and ord("q") != i:
-        print(chr(i), end="")
+    if i != 101  and 113 != i:
+        print("{:c}".format(i), end="")
