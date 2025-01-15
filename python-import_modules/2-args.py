301bada20befcc360ece093bb13c8f289845e68c
Cyprien <cypriengehu@example.com>
2025-01-15 14:57:02 +0000

    task 2 correction

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argc = len(argv)
 
     if argc == 1:
-        print("0 argument.")
+        print("0 arguments.")
     if argc == 2:
         print("1 argument:")
     if argc > 2:
