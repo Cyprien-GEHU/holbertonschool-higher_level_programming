e89c78dcf0fb246a51f1cfb08b0c4ee27a0334e7
Cyprien <cypriengehu@example.com>
2025-01-21 11:51:06 +0100

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     value = 0
     roman = 0
-    if roman_string is None:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     chiffre = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D':500, 'M': 1000}
     for roman in range(0, len(roman_string) - 1):
