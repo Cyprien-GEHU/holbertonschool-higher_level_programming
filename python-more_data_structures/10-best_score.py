7faea31cf280f8482e5fcb66c596244072c222c2
Cyprien <cypriengehu@example.com>
2025-01-22 15:54:47 +0000

    task 10 correction

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max = 0
+    maxium = 0
     name = ""
     for key in a_dictionary:
-        if max < a_dictionary[key]:
-            max = a_dictionary[key]
+        if maxium < a_dictionary[key]:
+            maxium = a_dictionary[key]
             name = key
     return name
