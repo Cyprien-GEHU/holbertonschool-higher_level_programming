14540676415d1fdd638df4e739044c0f9f58f00a
Cyprien <cypriengehu@example.com>
2025-01-23 08:58:38 +0000

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function who add 2 integer
+return a+b
+"""
+def add_integer(a, b=98):
+    """
+    the function add
+    """
+    if not isinstance(a, (float, int)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (float, int)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
