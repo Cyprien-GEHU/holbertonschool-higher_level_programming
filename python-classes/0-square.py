5bdbb55b37e2a7788fa4f967d6ad1f724b66d48f
Cyprien <cypriengehu@example.com>
2025-01-27 08:41:48 +0000

    task 0 and 1

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+We write a class square and is empty
+'''
+
+
+class Square:
+    """
+    Class Square :
+    the class are empty
+    """
---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+We write a class square:
+size : the private attribue
+'''
+
+
+class Square:
+    """
+    The class Square
+    """
+    def __init__(self, size):
+        self.__size = size
