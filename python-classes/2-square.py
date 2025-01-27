6b8bec41c8a0d04019c28cb32b15164b2356ca3e
Cyprien <cypriengehu@example.com>
2025-01-27 08:54:15 +0000

    task 2 and 3

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+We write a class square:
+size : the private attribue
+'''
+
+
+class Square:
+    """
+    The class Square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
