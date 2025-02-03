7b6de96e4d166dbb17f9dcb51e529c017db3915d
Cyprien <cypriengehu@example.com>
2025-02-03 12:01:55 +0000

    correctio task 10

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle(BaseGeometry):
     the class Rectangle
     """
     def __init__(self, width, height):
-        self().integer_validator("width", width)
-        self().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
