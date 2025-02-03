250f293342ae67d652d31e7d98490a4e9fc9e5df
Cyprien <cypriengehu@example.com>
2025-02-03 11:54:33 +0000

    correction and test

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     the class Square
     """
     def __init__(self, size):
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
