e1c9e18cfe51612d12140733fa6f56376599252d
Cyprien <cypriengehu@example.com>
2025-03-04 11:27:27 +0100

    correction

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list all genres and displaus the number of shows
-select tv_genres.name as genre,
-count(tv_show_genres.genre_id) as number_of_shows
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
