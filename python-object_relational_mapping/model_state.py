d4ebabbad8e99834e5f1e9822556a675f317a903
Cyprien <cypriengehu@example.com>
2025-03-05 10:43:22 +0100

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,9 +12,10 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-    if __name__ == "__main__":
-        engine = create_engine("mysql+mysqldb://{};{}@localhost/{}"
-                               .format(sys.argv[1],
-                                       sys.argv[2],
-                                       sys.argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{};{}@localhost/{}"
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
