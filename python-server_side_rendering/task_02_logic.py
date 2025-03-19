cea6ad2dfb0df44dc0bcc6e446b06d3735c5b18e
Cyprien <cypriengehu@example.com>
2025-03-19 15:47:21 +0100

    task 2 correction

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -4,9 +4,11 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/items')
 def items():
@@ -16,13 +18,16 @@ def items():
 
     return render_template('items.html', items=items_list)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
